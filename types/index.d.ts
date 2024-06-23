a5fabf7ffe75ec2c592a02a79556eeb5594eca83
renorari <renorari@renorari.net>
2024-06-23 09:04:26 +0900

    ✨ Discord Appender

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,15 @@
-export interface SlackAppender {
-        type: '@log4js-node/slack';
-        // your Slack API token (see the slack docs)
-        token: string;
-        // the channel to send log messages
-        channel_id: string;
+export interface DiscordAppender {
+        type: 'log4js-discord';
+        // your Discord Webhook URL (see the discord docs)
+        url: string;
         // the icon to use for the message
-        icon_url?: string;
+        iconUrl?: string;
         // the username to display with the message
         username: string;
+        // the channel to send the message to
+        color: string;
         // (defaults to basicLayout) - the layout to use for the message.
         layout?: Layout;
 }
 
-export type Appender = Appender | SlackAppender;
+export type Appender = Appender | DiscordAppender;
